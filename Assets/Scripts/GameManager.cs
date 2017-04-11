1c516fc1d92923b212424a82d41ec71056e29f51
omriklein <omrik2007@gmail.com>
2017-04-11 20:51:14 +0300

    First commit
    Let's get started

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+enum things
+{
+    solider,
+    tank,
+    airplain,
+    nothing
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] soldiers;
+
+    public GameObject target = null;
+    private InputField input; // the input field
+
+    // Use this for initialization
+    void Start()
+    {
+        soldiers = GameObject.FindGameObjectsWithTag("Soldier");
+        input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.Select(); // set the cursor to the input field
+
+        // get all the soldiers in the game
+        foreach (GameObject s in soldiers)
+        {
+            //s.GetComponent<Soldier>().walkTo(target.transform.position);
+        }
+
+        // on submit input field
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //print(input.text); // print the text
+            interpator(input.text);
+            // S9becon = 9 soldiers to becon
+            input.text = ""; // zeroize the input field text
+            input.ActivateInputField(); // activate the input field
+        }
+    }
+
+    private void interpator(string text)
+    {
+        things thing;
+        int number = 0;
+
+        if (text.Length == 0)
+        { print("Error..."); return; }
+
+        switch (text[0])
+        {
+            case 'S': thing = things.solider; break;
+            case 'T': thing = things.tank; break;
+            case 'A': thing = things.airplain; break;
+            default: thing = things.nothing; break;
+        }
+
+        text = text.Substring(1);
+        while (text.Length > 0 && text[0] >= '0' && text[0] <= '9')
+        {
+            number = number * 10 + ((int)text[0] - 48); // '0' is 48 in unicode
+            text = text.Substring(1);
+        }
+
+        // sund "number" of "thing" to "place" ~ "text"
+        print(thing + ", " + number + ", " + text);
+
+        try
+        {
+            for (int i = 0; i < number; i++)
+            {
+                soldiers[i].GetComponent<Soldier>().targetObject = GameObject.Find(text);
+            }
+        }
+        catch { print("Error is found... cannot walk"); }
+    }
+}
---
+++ Assets/Scripts/Soldier.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Soldier : MonoBehaviour
+{ 
+    //turn to 'private'
+    public const float MAX_HP = 100; // the maximum HP
+    public float health; // the current HP
+    public bool isAttack; // if this soldier is attacking
+    public float speed = 0.1f; // the walking speed
+
+    public Vector3 testPoint;
+    public GameObject targetObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        health = MAX_HP;
+        isAttack = false; // turn to private
+        targetObject = null; // turn to private
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetObject != null)
+            walkTo(targetObject.transform.position);
+    }
+
+    public void walkTo(Vector3 target)
+    {
+        if (this.transform.position != target)
+        {
+            this.transform.LookAt(target);
+            this.transform.Translate(Vector3.forward * Mathf.Clamp(Vector3.Distance(this.transform.position, target), 0, speed));
+        }
+        else
+        {
+            this.targetObject = null;
+        }
+    }
+
+    private void walkTo2(Vector3 target)
+    {
+        if (this.transform.position != target)
+        {
+            this.transform.LookAt(testPoint);
+            if (Vector3.Distance(this.transform.position, target) >= speed)
+                this.transform.Translate(Vector3.forward * speed);
+            else
+                this.transform.position = target;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(targetObject != null && collision.gameObject.name==targetObject.name)
+        {
+            targetObject = null;
+        }
+        else if (collision.gameObject.tag == "Soldier")
+        {
+            this.transform.Translate(Vector3.right * speed);
+        }
+    }
+}
